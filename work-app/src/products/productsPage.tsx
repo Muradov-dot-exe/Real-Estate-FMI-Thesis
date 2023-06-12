2b7c6225811cfe93f9d3dae9508f155907d5f383
Ali Muradov <73521725+Muradov-dot-exe@users.noreply.github.com>
2023-06-12 13:44:18 +0300

    created base table reusable component

--- work-app/src/products/productsPage.tsx
+++ work-app/src/products/productsPage.tsx
@@ -1,5 +1,26 @@
+import { Box, Divider, Typography } from "@mui/material";
+import DataTableComponent from "../components/DataTableComponent";
+
 const ProductsPages = () => {
-  return <p>Welcome to products</p>;
+  return (
+    <>
+      <Box
+        sx={{
+          marginTop: 5,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginBottom: 5,
+        }}
+      >
+        <Typography sx={{ fontWeight: "bold" }}>Available Products</Typography>
+      </Box>
+      <Divider />
+      <Box>
+        <DataTableComponent />
+      </Box>
+    </>
+  );
 };
 
 export default ProductsPages;
