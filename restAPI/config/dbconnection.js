ea64b42ab443112a656ac62325728a3011a0fa01
Ali Muradov <amuradov239@gmail.com>
2024-06-15 11:15:13 +0300

    Explicitly specified the host

--- restAPI/config/dbconnection.js
+++ restAPI/config/dbconnection.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: process.env.DB_HOST,
+    host: "127.0.0.1",
     port: process.env.DB_PORT,
     dialect: "mysql",
     dialectModule: require("mysql2"),
