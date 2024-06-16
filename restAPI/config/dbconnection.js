9761ac2ac766d84b7e5d250babb68582691bdef4
Ali Muradov <amuradov239@gmail.com>
2024-06-16 12:02:46 +0300

    changed the host successfully

--- restAPI/config/dbconnection.js
+++ restAPI/config/dbconnection.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: "127.0.0.1",
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "mysql",
     dialectModule: require("mysql2"),
